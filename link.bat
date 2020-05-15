fd48704c5d8150fd00ed6d51bf1e8659fbb7cf29
Five <xianzhe0825@gmail.com>
2020-05-15 15:23:59 +0800

    资源仓库

---
+++ link.bat
@@ -0,0 +1,13 @@
+set src=%CD%\
+set dst=C:\Project\pokerCard\client\Moonlight\Assets\BundleResources\
+
+
+mklink /D "%dst%AAA Projectiles" "%src%Resources\AAA Projectiles"
+
+mklink /D "%dst%Animation" "%src%Resources\Animation"
+
+mklink /D "%dst%Models" "%src%Resources\Models"
+
+mklink /D "%dst%ModularRPGHeroesPolyArt" "%src%Resources\ModularRPGHeroesPolyArt"
+
+mklink /D "%dst%PolygonFarm" "%src%Resources\PolygonFarm"
